283cb6a732bd44e30eb6195b9f19b8235f16ec10
terrysducks <41840392+terrysducks@users.noreply.github.com>
2019-04-15 18:54:24 -0400

    woop

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,11 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+
 import "./bootstrap.min.css"
-
 import "./layout.css"
-import Navbar from "./Globals/Navbar"
 
+import Navbar from "./Globals/Navbar"
 const Layout = ({ children }) => (
   <>
     <Navbar />
